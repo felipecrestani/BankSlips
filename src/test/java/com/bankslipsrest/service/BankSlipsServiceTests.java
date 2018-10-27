f05b9c0518b36f920897cb3c4e2e318d121d0d0d
felipecrestani <fcrestanihc@hotmail.com>
2018-10-27 19:11:50 -0300

    Change to BankSlipsDTO

--- src/test/java/com/bankslipsrest/service/BankSlipsServiceTests.java
+++ src/test/java/com/bankslipsrest/service/BankSlipsServiceTests.java
@@ -4,10 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 import com.bankslipsrest.DemoApplicationTests;
 import com.bankslipsrest.dto.BankSlipsDTO;
+import com.bankslipsrest.dto.BankSlipsPayDTO;
 import com.bankslipsrest.dto.BankSlipsPostDTO;
 import com.bankslipsrest.entity.BankSlips;
 import com.bankslipsrest.entity.BankSlipsPaymentStatus;
@@ -23,15 +25,22 @@ public class BankSlipsServiceTests extends DemoApplicationTests {
 	@Autowired
 	private BankSlipsService bankSlipsService;
 
+	private BankSlips bankSlips = null;
 	private BankSlipsDTO bankSlipsDTO = null;
 	private BankSlipsPostDTO bankSlipsPostDTO = null;
+	private BankSlipsPayDTO bankSlipsPayDTO = null;
 
 	@Before
 	public void setUp() {
 		bankSlipsPostDTO = new BankSlipsPostDTO();
-		bankSlipsPostDTO.setCustomer("Dents Company S.A");
+		bankSlipsPostDTO.setCustomer("Test Company S.A");
 		bankSlipsPostDTO.setDueDate("2018-10-01");
 		bankSlipsPostDTO.setTotalInCents(BigDecimal.valueOf(1000));
+
+		bankSlipsPayDTO = new BankSlipsPayDTO();
+		bankSlipsPayDTO.setPaymentDate("2018-10-01");
+
+		this.bankSlipsService.createBankSlips(this.bankSlipsPostDTO);
 	}
 
 	@Test
@@ -51,9 +60,34 @@ public class BankSlipsServiceTests extends DemoApplicationTests {
 		assertBankSlips(bankSlipsDTO,bankSlipsDTO.getId(), bankSlipsDTO.getTotalInCents(), bankSlipsDTO.getDueDate(), bankSlipsDTO.getStatus());
 	}
 
+	@Test
+	@Transactional
+	public void BankSlipsServicePay() throws Exception {
+		this.bankSlipsDTO = this.bankSlipsService.createBankSlips(this.bankSlipsPostDTO);
+		this.bankSlips = this.bankSlipsService.payBankSlips(UUID.fromString(this.bankSlipsDTO.getId()), this.bankSlipsPayDTO);
+		assertThat(bankSlips.getStatus().equals(BankSlipsPaymentStatus.PAID));
+	}
+
+	@Test
+	@Transactional
+	public void BankSlipsServiceDelete() throws Exception {
+		this.bankSlipsDTO = this.bankSlipsService.createBankSlips(this.bankSlipsPostDTO);
+		this.bankSlips = this.bankSlipsService.cancelBankSlips(UUID.fromString(this.bankSlipsDTO.getId()));
+		assertThat(bankSlips.getStatus().equals(BankSlipsPaymentStatus.CANCELED));
+	}
+
+	@Test
+	@Transactional
+	public void BankSlipsServiceGetAll() throws Exception {
+		this.bankSlipsService.createBankSlips(this.bankSlipsPostDTO);
+		this.bankSlipsService.createBankSlips(this.bankSlipsPostDTO);
+		List<BankSlipsDTO> list = this.bankSlipsService.getAll();
+		assertThat(list.size() == 2);
+	}
+
 	private void assertBankSlips(BankSlipsDTO saved, String id, BigDecimal totalInCents, String dueDate, String status) {
 		assertThat(id.length() == 36);
-		assertThat(saved.getCustomer()).isEqualTo("Dents Company S.A");
+		assertThat(saved.getCustomer()).isEqualTo("Test Company S.A");
 		assertThat(saved.getTotalInCents()).isEqualTo(totalInCents);
 		assertThat(saved.getDueDate()).isEqualTo(dueDate);
 		assertThat(saved.getStatus()).isEqualTo(status);
